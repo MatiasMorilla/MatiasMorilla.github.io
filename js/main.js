fd6d25a8cff61f263cd06c6ad4d50606a3443dd9
MatiMorilla <matiasmorilla@hotmail.com>
2021-08-28 00:10:15 -0300

    terminando header mobile

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-const header = document.querySelector(".header");
-const header_nav = document.querySelector(".header__nav");
-const btn_menu = document.querySelector(".btn_menu");
+const header = $(".header");
+const header_nav = $(".header__nav");
+const btn_menu = $(".btn_menu");
 
 window.onscroll = function()
 {
     let y = window.scrollY;
     if(y >= 60)
     {
-        header.classList.add("header--fixed");
+        header.addClass("header--fixed");
     }
     else
     {
-        header.classList.remove("header--fixed");
+        header.removeClass("header--fixed");
     }
 }
 
-btn_menu.addEventListener("click", function()
+btn_menu.click(function()
 {
-    /* header.classList.toggle("header--fixed"); */
-    header_nav.classList.toggle("header--show");
-})
+    header_nav.toggleClass("header--show");
+});
